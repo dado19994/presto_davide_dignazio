481af20afb414211895ae818e3bef7405b226d9b
dado19994 <dignazio90@gmail.com>
2026-05-10 12:36:02 +0200

    User Story 3 completata

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -23,26 +23,28 @@ class RevisorController extends Controller
     {
         $article->setAccepted(true);
         return redirect()
-        ->back()
-        ->with('message', 'Articolo accettato con successo ' . $article->title);
-        }
+            ->back()
+            ->with('message', 'Articolo accettato con successo ' . $article->title);
+    }
 
     public function reject(Article $article)
     {
         $article->setAccepted(false);
         return redirect()
-        ->back()
-        ->with('message', 'Articolo rifiutato con successo ' . $article->title);
+            ->back()
+            ->with('message', 'Articolo rifiutato con successo ' . $article->title);
     }
 
-    public function becomeRevisor(){
+    public function becomeRevisor()
+    {
         Mail::to('admin@example.com')->send(new BecomeRevisor(Auth::user()));
         return redirect()->route('homepage')->with('message', 'Richiesta inviata con successo');
-
     }
 
-    public function makeRevisor(User $user){
+    public function makeRevisor(User $user)
+    {
         Artisan::call('app:make-user-revisor', ['email' => $user->email]);
-        return redirect()->back();
+        return redirect()->route('homepage')
+            ->with('message', 'Utente nominato revisore con successo');
     }
 }
